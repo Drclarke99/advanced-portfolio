7c479842124ead6ac4213d06fea18dee8bda2cf2
David Clarke <drclarke99@gmail.com>
2023-01-20 19:24:59 -0800

    added emailjs integration

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList.add("modal__overlay--visible");
+
+    emailjs.sendForm(
+        'service_ksa4da8',
+        'template_jo6kgth',
+        event.target,
+        'feXUToQm4IFaMM4m-'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList.add("modal__overlay--visible");
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on drclarke99@gmail.com"
+        );
+    });
+}
